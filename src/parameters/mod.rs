e26788497f1b2344ca84f1d6fa667a6fcf3fd359
Kobi Gurkan <kobigurk@gmail.com>
2020-04-19 20:09:22 +0300

    adds benchmark for 60 bit primes

--- src/parameters/mod.rs
+++ src/parameters/mod.rs
@@ -45,6 +45,36 @@ impl Parameters {
         Ok((parameters, security_level))
     }
 
+    // based on page 33 in https://eprint.iacr.org/2019/1255.pdf
+    pub fn from_curve_and_small_prime_size<P: Field>(prime_bits_min: u16, prime_bits_max: u16) -> Result<(Parameters, u16), ParametersError> {
+        let field_size_bits = P::size_in_bits() as u16;
+        let security_level = field_size_bits/2;
+        let derived = (|| {
+            for c in 0..security_level {
+                let security_soundness_zk = ((2*security_level - 2 - c) - 2)/2;
+                for i in prime_bits_min..=prime_bits_max {
+                    if i <= 2*security_level - 2 - c && (2*security_level - 2 - c) % i >= i - c {
+                       return Some((i, security_soundness_zk));
+                    }
+                }
+            }
+
+            return None;
+        })();
+        let (prime_bits, security_soundness_zk) = derived.ok_or(ParametersError::InvalidParameters)?;
+
+        let parameters = Parameters {
+            security_level,
+            security_zk: security_soundness_zk,
+            security_soundness: security_soundness_zk,
+            field_size_bits,
+            hash_to_prime_bits: prime_bits,
+        };
+
+        parameters.is_valid()?;
+        Ok((parameters, security_level))
+    }
+
     pub fn is_valid(&self) -> Result<(), ParametersError> {
         // See page 32 in https://eprint.iacr.org/2019/1255.pdf
         let d = 1 + (self.security_zk + self.security_soundness + 2)/self.hash_to_prime_bits;
